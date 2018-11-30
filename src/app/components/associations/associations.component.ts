dba9d687e4d0ce11f8d67cec0d3563bcc81916c8
Nemanja Juric <nemanjajuric92@gmail.com>
2018-11-30 20:27:16 +0100

    commit

--- src/app/components/associations/associations.component.ts
+++ src/app/components/associations/associations.component.ts
@@ -1,22 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { QuizService } from 'src/app/services/quiz.service';
-import { faColumns } from '@fortawesome/free-solid-svg-icons';
+import { faQuestionCircle, faArrowLeft } from '@fortawesome/free-solid-svg-icons';
+import { SafeHtml, DomSanitizer } from '@angular/platform-browser';
+import { TaskHelper } from 'src/app/helpers/task.helper';
 
 @Component({
   selector: 'app-associations',
   templateUrl: './associations.component.html',
 })
-export class AssociationsComponent implements OnInit {
+export class AssociationsComponent implements OnInit, OnDestroy {
 
   constructor(
-    private _quizService: QuizService
+    private _quizService: QuizService,
+    private _sanitizer: DomSanitizer,
   ) { }
+
+  faQuestionCircle = faQuestionCircle;
+  faArrowLeft = faArrowLeft;
 
   game: any;
   final: string = 'Коначно';
+  showWindow: boolean = false;
+  question: SafeHtml;
+  answer: SafeHtml;
+  renderedText: SafeHtml;
+  private _isQuestionShown: boolean = false;
 
   ngOnInit() {
     this._getGame();
+    this._quizService.inGame(true);
+  }
+
+  ngOnDestroy() {
+    this._quizService.inGame(false);
   }
 
   private _getGame() {
@@ -33,17 +49,46 @@ export class AssociationsComponent implements OnInit {
   }
 
   showQuestion(field) {
-    field.value = field.term;
-    field.opened = true;
+    if (!field.opened) {
+      field.value = field.term;
+      field.opened = true;
+      this.question = this._sanitizer.bypassSecurityTrustHtml(TaskHelper.makeMathText(field.question));
+      this.answer = this._sanitizer.bypassSecurityTrustHtml(TaskHelper.makeMathText(field.answer));
+      this.renderedText = this.question;
+      this._isQuestionShown = true;
+      this.showWindow = true;
+    } else {
+      field.value = field.name;
+      field.opened = false;
+    }
   }
 
-  showTerm(column){
-    column.value = column.solution;
-    column.opened = true;
+  showTerm(column) {
+    if (!column.opened) {
+      column.value = column.solution;
+      column.opened = true;
+    } else {
+      column.value = column.name;
+      column.opened = false;
+    }
   }
 
-  finalSolution(){
+  finalSolution() {
     this.final = this.game.solution;
+  }
+
+  showAssociations() {
+    this.showWindow = false;
+  }
+
+  showAnswer() {
+    if (this._isQuestionShown) {
+      this.renderedText = this.answer;
+      this._isQuestionShown = false;
+    } else {
+      this.renderedText = this.question;
+      this._isQuestionShown = true;
+    }
   }
 
 }
