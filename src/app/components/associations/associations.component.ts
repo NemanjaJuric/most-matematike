10a63381c1ad2f8d1abab95d640dc7ec520cf4d0
Nemanja Juric <nemanja.juric@finbet.com>
2018-11-30 15:49:24 +0100

    commit

--- src/app/components/associations/associations.component.ts
+++ src/app/components/associations/associations.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { QuizService } from 'src/app/services/quiz.service';
+import { faColumns } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-associations',
@@ -12,16 +13,37 @@ export class AssociationsComponent implements OnInit {
   ) { }
 
   game: any;
+  final: string = 'Коначно';
 
   ngOnInit() {
     this._getGame();
   }
 
-  private _getGame(){
+  private _getGame() {
     this._quizService.getGame()
-    .subscribe(g => {
-      this.game = g;
-    })
+      .subscribe(g => {
+        g.columns.forEach(column => {
+          column.value = column.name;
+          column.fields.forEach(field => {
+            field.value = field.name;
+          });
+        });
+        this.game = g;
+      })
+  }
+
+  showQuestion(field) {
+    field.value = field.term;
+    field.opened = true;
+  }
+
+  showTerm(column){
+    column.value = column.solution;
+    column.opened = true;
+  }
+
+  finalSolution(){
+    this.final = this.game.solution;
   }
 
 }
